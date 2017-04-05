c7494760725c7cfb905cb31e7b757e59432f3769
mayreeh <shirrohmary@gmail.com>
2017-04-05 11:44:15 +0300

    identifying  if certain words exist in array given

--- Basic Algorithm Scripting/mutations.js
+++ Basic Algorithm Scripting/mutations.js
@@ -11,23 +11,27 @@ Lastly, ["Alien", "line"], should return true because all of the letters in "lin
 
 
 function mutation(arr) {
-  var vv = 1;
-  var arrOne = arr[0]
-  var arrTwo = arr[1].split('');
-  arrTwo.map(function(num){ return arrOne.indexOf(num) > 0 ?  vv += 1 : vv += 0; });
-  return vv == arrTwo.length ? true : false;
+  var vv = 0;
+  var arrOne = arr[0].toLowerCase();
+  var arrTwo = arr[1].toLowerCase().split('');
+  arrTwo.map(function(num){ return arrOne.indexOf(num) > -1 ?  vv += 0 : vv += 1; });
+  console.log(vv ,  arrTwo.length );
+  return vv === 0 ? true : false;
 
 
 }
 
-console.log(mutation(["hello", "hey"]));
+console.log(mutation(["Mary", "Aarmy"]) );
 
 /*
 EXPLANATION
-indexOf() is a function for checking if a certain element in the array exist.
-so we need to loop through the second array , to get if each element exist, make each character an array by using split method
-Loop using map to get if each character exist, if it does , add one, if it doesnt add zeroth
-to identify if the whole word exist, compare its length with the total number of EXISTENCE.
-if equal, then the whole word exist.
+indexOf() is a function for checking if a certain element in the array exist. (In Jqery we use inArray())
+1.To avoid troubles make both arrays to lower case - toLowerCase() function does that.
+2.Access the second array arr[1] which is the second argument and create an array of each character -split() does that.
+  so we need to loop through the second array , to get if each element exist, make each character an array by using split method
+  Loop using map to get if each character exist, if it doesNOT indexOf() returns -1 , hence if it does it should return value more than -1
+
+3.So if it exists , keep adding zero , so that the result will be zero otherwse keep adding one
+4.If it exists the result should be zero hence TRUE otherwise if not Zero, then return false
 
 */
